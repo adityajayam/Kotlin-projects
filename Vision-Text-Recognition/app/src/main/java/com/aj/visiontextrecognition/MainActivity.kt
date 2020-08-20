13cfdaef3e10ac2b289afe7b214961c044063393
Aditya Jayam <52209094+adityajayam@users.noreply.github.com>
2020-08-21 00:15:24 +0530

    request_permission
    basic layout structure and request permission.

---
+++ Vision-Text-Recognition/app/src/main/java/com/aj/visiontextrecognition/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.aj.visiontextrecognition
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var cameraPreview: PreviewView? = null
+    private lateinit var takePhoto: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initialize();
+        if (checkAllPermissionGranted()) {
+            //TODO call camera
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_CAMERA_PERMISSION
+            )
+        }
+    }
+
+    private fun initialize() {
+        cameraPreview = findViewById(R.id.camera_preview)
+        takePhoto = findViewById(R.id.take_photo)
+        takePhoto.setOnClickListener { Log.e(TAG, "button click") }
+    }
+
+    private fun checkAllPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            this,
+            it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_CAMERA_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //TODO start camera
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Permissions not granted.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val REQUEST_CODE_CAMERA_PERMISSION = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+}
