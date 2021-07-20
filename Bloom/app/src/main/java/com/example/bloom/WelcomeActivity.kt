bb5b1215d3557e2aafb5d60fa98e2bcf82a04b1d
Aditya Jayam <52209094+adityajayam@users.noreply.github.com>
2021-07-20 14:27:26 +0530

    login

--- Bloom/app/src/main/java/com/example/bloom/WelcomeActivity.kt
+++ Bloom/app/src/main/java/com/example/bloom/WelcomeActivity.kt
@@ -1,11 +1,24 @@
 package com.example.bloom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bloom.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var activityWelcomeBinding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        activityWelcomeBinding = ActivityWelcomeBinding.inflate(this.layoutInflater)
+        setContentView(activityWelcomeBinding.root)
+        activityWelcomeBinding.apply {
+            welcomeActivity = this@WelcomeActivity
+            lifecycleOwner = lifecycleOwner
+        }
+    }
+
+    fun launchLoginActivity() {
+        val launch = Intent(this, LogIn::class.java)
+        startActivity(launch)
     }
 }
